fedf99b0977727eccbacfafde68fae54390dd3e7
Titouan Goubet <titouangoubet@hotmail.fr>
2024-09-25 14:05:39 +0200

    Initialisation du repo backend

---
+++ src/test/java/com/example/auth_form/AuthFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
